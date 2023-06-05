466ffedb461b4acbf7243082bfb724c02233d361
hempalS39 <hs527196@gmail.com>
2023-06-05 15:54:48 +0530

    final-branch

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,22 +1,24 @@
 const urlModel = require('../models/urlModel');
 const shortid = require('shortid');
 const validator = require('validator');
+const isURL = require('is-url');
+require('dotenv').config();
+const {REDIS_HOST ,REDIS_PORT, REDIS_PASSWORD} = process.env
 
 const redis = require('redis');
 const { promisify } = require("util");
 
 //creating redis client
-const redisClient = redis.createClient(
-    19265,
-    "redis-19265.c14.us-east-1-2.ec2.cloud.redislabs.com",
-  { no_ready_check: true }
-);
-
-
-redisClient.auth("CSAOlSLpfR53XKWOXjwcrqUtgReTxlwC", function (err) {
-  if (err) throw err;
+const redisClient = redis.createClient({
+   port : REDIS_PORT,
+   host : REDIS_HOST,
+   password : REDIS_PASSWORD
+//   { no_ready_check: true }
 });
 
+// redisClient.auth("CSAOlSLpfR53XKWOXjwcrqUtgReTxlwC", function (err) {
+//   if (err) throw err;
+// });
 
 redisClient.on("connect", async function () {
   console.log("Connected to Redis");
@@ -30,57 +32,69 @@ const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 
 function isValid(value) {
+    if(value == null || value == undefined) return false;
     if(typeof value == "string" && value.trim() == "") return false;
     return true
 }
 
-
-const shortUrl = async (req , res) => {
+// Ist api for generating shortUrl
+const shortUrl = async (req, res) => {
     try {
         let longUrl = req.body.longUrl
+        // Validating Url
+        if (Object.keys(req.body).length == 0) return res.status(400).send({ status: false, message: "pls enter url" })
 
-    if(Object.keys(req.body).length == 0) return res.status(400).send({status : false , message : "pls enter url"})
-    
-    if(!isValid(longUrl)) return res.status(400).send({status : false , msg : "invalid format longUrl"})
+        if (!isURL(longUrl)) return res.status(400).send({ status: false, msg: "not a valid longUrl" })
 
-    if(!validator.isURL(longUrl)) return res.status(400).send({status : false , msg : "invalid longUrl"})
-    
-    //caching
-    const cachedUrl = await GET_ASYNC(`${longUrl}`);
-    // console.log(`cachedUrl : ${cachedUrl}`)
-    if (cachedUrl) {
-        // const { longUrl } = JSON.parse(cachedUrl);
-        return res.send(JSON.parse(cachedUrl))
-    }
+        if (!isValid(longUrl)) return res.status(400).send({ status: false, msg: "invalid format longUrl" })
 
-    
-    let urlData = await urlModel.findOne({longUrl: longUrl}).select({longUrl:1, shortUrl:1, urlCode:1 ,_id :0})
-    await SET_ASYNC(`${longUrl}`, JSON.stringify(urlData));
-    if(urlData) return res.status(200).send({status : true , data : urlData});
-    
-    console.log(`urlData : ${urlData}`)
+        if (!validator.isURL(longUrl)) return res.status(400).send({ status: false, msg: "invalid longUrl" })
 
-    const urlCode = shortid.generate();
-    const baseUrl = "localhost:3000" ;
-    const shortUrl = baseUrl + "/" + urlCode ;
+        // finding url data in cache 
+        const cachedUrl = await GET_ASYNC(`${longUrl}`);
+        if (cachedUrl) {
+            return res.status(200).send({ status: true, data: JSON.parse(cachedUrl) });
+        }
 
-    const urlDataToSave = {longUrl, shortUrl, urlCode }
+        //finding url data in database
+        let urlData = await urlModel.findOne({ longUrl: longUrl }).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 })
 
-    const savedUrl =  await urlModel.create(urlDataToSave)
+        if (urlData) {
+            await SET_ASYNC(`${longUrl}`, JSON.stringify(urlData),
+                'EX',
+                24 * 60 * 60
+            );
 
-    let urlResponse = {
-        longUrl : savedUrl.longUrl,
-        shortUrl: savedUrl.shortUrl,
-        urlCode : savedUrl.urlCode
-    } 
+            return res.status(200).send({ status: true, data: urlData });
+        }
+        // console.log(`urlData : ${urlData}`)
 
-    console.log(`newdata : ${urlResponse}`)
-    //setting urldata in caching
-     await SET_ASYNC(`${longUrl}`, JSON.stringify(urlResponse));
+        // creating new shortUrl
+        const urlCode = shortid.generate();
+        const baseUrl = "localhost:3000";
+        const shortUrl = baseUrl + "/" + urlCode;
 
-    res.status(201).send({status : true , data : urlResponse})
+        const urlDataToSave = { longUrl, shortUrl, urlCode }
+        //saving new urlData in datavase
+        const savedUrl = await urlModel.create(urlDataToSave);
+
+        //fetching urldata to be send in response & to be set in cache
+        let urlResponse = {
+            longUrl: savedUrl.longUrl,
+            shortUrl: savedUrl.shortUrl,
+            urlCode: savedUrl.urlCode
+        }
+        // console.log(`newdata : ${urlResponse}`)
+
+        //setting urldata in caching
+        await SET_ASYNC(`${longUrl}`, JSON.stringify(urlResponse),
+            'EX',
+            24 * 60 * 60
+        );
+        // sending url data in response if url data is created first time
+        return res.status(201).send({ status: true, data: urlResponse })
     } catch (error) {
-        return res.status(500).send({status : false , message : error.message})
+        return res.status(500).send({ status: false, message: error.message })
     }
 }
 
@@ -95,25 +109,28 @@ const getShortUrl = async (req, res) => {
 
         //finding url in cache
         const cachedUrl = await GET_ASYNC(`${urlCode}`);
-        console.log(`cachedUrl : ${cachedUrl}`)
-        //redirect url if found in cache
-        if (cachedUrl) {
-            const { longUrl } = JSON.parse(cachedUrl);
-            return res.status(302).redirect(longUrl)
+        //if find in cache redirect longUrl 
+        if(cachedUrl){
+            return res.status(302).redirect(JSON.parse(cachedUrl))
         }
-
+    
+        
         //finding url in database
-        const url = await urlModel.findOne({urlCode});
-        console.log(`url: ${url}`)
+        const url = await urlModel.findOne({urlCode : urlCode}).select({_id : 0});
         if (!url) {
             return res.status(404).json({
                 status: false,
                 message: 'URL not found',
             });
         }
-        //seting url in cache
-        await SET_ASYNC(`${urlCode}`, JSON.stringify({ longUrl: url.longUrl }))
 
+        //seting url in cache
+        await SET_ASYNC(`${urlCode}`, JSON.stringify(url.longUrl),
+            'EX',
+            24 * 60 * 60
+        )
+
+        // redirecting longUrl
         return res.status(302).redirect(url.longUrl)
   
     } catch (error) {
@@ -123,35 +140,9 @@ const getShortUrl = async (req, res) => {
         })
     }
 }
-// const getShortUrl = async function (req, res) {
-//     try {
-//       const { urlCode } = req.params;
-  
-//       // Check if the URL exists in the cache
-//       let cachedUrl = await GET_ASYNC(urlCode);
-//       if (cachedUrl) {
-//         const { longUrl } = JSON.parse(cachedUrl);
-//         // Redirect to the original URL
-//         return res.redirect(longUrl);
-//       }
-  
-//       // Find the URL document in the database
-//       const url = await urlModel.findOne({ urlCode: urlCode });
-  
-//       if (url) {
-//         // Cache the URL for 24 hours
-//         await SET_ASYNC(urlCode, JSON.stringify({ longUrl: url.longUrl }), 'EX', 24 * 60 * 60);
-//         // Redirect to the original URL
-//         return res.redirect(url.longUrl);
-//       } else {
-//         // URL not found in the cache or database
-//         return res.status(404).send({ error: 'URL not found' });
-//       }
-//     } catch (err) {
-//       console.error(err);
-//       return res.status(500).send({ error: 'Server Error' });
-//     }
-//   };
   
 module.exports.shortUrl = shortUrl;
 module.exports.getShortUrl = getShortUrl;
+
+
+
